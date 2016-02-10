7cbedcc393be7cb9cf27e2580243bfe1b9dcedbf
verdastelo <verda_stelo@hotmail.com>
2016-02-10 06:53:19 +0530

    Exercise 8 solved.

---
+++ ch4/exercises/exercise7.cpp
@@ -0,0 +1,5 @@
+/*
+    Need to solve exercise 6 first.
+    Feb. 10, 06:11. 
+*/ 
+
---
+++ ch4/exercises/exercise8.cpp
@@ -0,0 +1,37 @@
+/*
+    A king rewards the inventor of chess.
+*/
+
+#include <iostream>
+#include <cmath>
+#include <vector>  
+
+using namespace std; 
+
+int main()
+{
+    int square = 1; // There can't be a zero square. 
+    int rice_grain = 1; // The inventor asks for at least one grain. 
+    cout << "square " << '\t' << "rice grains" << '\n'; 
+    for (int i = 1; i < 64; i++) {
+        cout << square << '\t' << rice_grain << '\n';
+        rice_grain = rice_grain * 2; 
+        square++;
+        
+        if ((rice_grain > 1000) && (rice_grain < 2000)) {
+            cout << "The inventor will have a 1000 grains of "
+                 << "rice on square " << square << endl; 
+                 }     
+        if ((rice_grain > 1000000) && (rice_grain < 2000000)) {
+            cout << "The inventor will have a 1000000 grains of "
+                 << "rice on square " << square << endl; 
+                 }
+              
+        if ((rice_grain > 1000000000) && (rice_grain < 2000000000)) {
+            cout << "The inventor will have a 1000000000 grains of "
+                 << "rice on square " << square << endl; 
+                 }
+        } 
+        
+    return 0; 
+} 
