b82be8c2bfb2b9c066e2fa66eb683e45200285d8
verdastelo <verda_stelo@hotmail.com>
2016-02-11 07:16:57 +0530

    Working on exercise 10.

--- ch4/exercises/exercise10.cpp
+++ ch4/exercises/exercise10.cpp
@@ -1,3 +1,59 @@
 /*
-    Need to return to it later.
+    Create a rock, papers and scissors game. 
 */
+
+#include <iostream> 
+#include <vector> 
+
+using namespace std; 
+
+int main()
+{
+    cout << "東京タワーを知っていますか。\n"; 
+    cout << "Let's play rock, paper and scissors.\n"; 
+    cout << "Enter (r)ock, (p)aper and (s)cissors to play.\n";
+    cout << "All other values will be rejected.\n"; 
+    
+    char c = ' '; // user input
+    char res = 's'; // random answer by the program
+    
+    vector <char> responses;
+    char user_input = ' '; 
+    
+    while (cin >> user_input) {
+        if ((user_input == 's') or 
+            (user_input == 'p') or 
+            (user_input == 'r')) {
+            
+            responses.push_back(user_input); 
+            }
+        
+        else 
+            cout << "Thank you!\n";
+            break; 
+        } 
+    
+    while (cin >> c) {
+        switch (c) {
+            case 'r': 
+                if (res == 'p') cout << "It's a p. You lose!\n";
+                if (res == 's') cout << "It's an s. You win!\n";
+                if (res == 'r') cout << "It's an r. A draw!\n"; 
+                break; 
+            case 'p':
+                if (res == 'p') cout << "It's a p. A draw!\n";
+                if (res == 's') cout << "It's an s. You lose!\n";
+                if (res == 'r') cout << "It's an r. A win!\n";
+                break;
+            case 's': 
+                if (res == 'p') cout << "It's a p. You win!\n";
+                if (res == 's') cout << "It's an s. A draw!\n";
+                if (res == 'r') cout << "It's an r. You lose!\n";
+                break;
+            default: 
+                cout << "Please enter a valid input.\n"; 
+                break;
+            } 
+            }
+      return 0; 
+} 
