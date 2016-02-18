951bf8fa0186269047013cb92b192736a1ec6f76
verdastelo <verda_stelo@hotmail.com>
2016-02-18 21:51:14 +0530

    exercise12.cpp finds all prime numbers between 7 and your input.

---
+++ ch4/exercises/exercise12.cpp
@@ -0,0 +1,56 @@
+#include <iostream> 
+#include <cmath> 
+#include <vector> 
+#include <algorithm> 
+
+// divide n by all the prime numbers
+// store the modulus in a vector
+// sort the vector 
+// if modulus is zero for the first number, n is not a prime
+// if it not, n is a prime 
+
+using namespace std; 
+
+int finds_a_prime(int n)
+{
+    vector <int> primes; 
+    
+    primes.push_back(2);
+    primes.push_back(3);
+    primes.push_back(5);
+
+    vector <double> check_primes; 
+    double prime_store; 
+    
+    if ((n % 2) == 0) {
+        // do nothing;
+        }
+    else 
+        {
+        for (int i = 0; i < primes.size(); i++) {
+            prime_store = (n % primes[i]); 
+            check_primes.push_back (prime_store); 
+            } 
+            
+        sort (check_primes.begin(), check_primes.end()); 
+        
+        if (check_primes[0] == 0) {
+            // do nothing;
+            }
+        else 
+            cout << n << " ";
+        }
+} 
+
+int main()
+{
+    cout << "Find prime numbers up to: ";
+    int n; 
+    cin >> n;
+    
+    for (int i = 5; i < n; i++) {
+        finds_a_prime(i);
+        }
+    cout << '\n'; 
+    return 0;
+} 
