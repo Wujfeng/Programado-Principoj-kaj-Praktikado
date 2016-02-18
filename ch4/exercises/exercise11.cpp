e48045c9196cbed9d9ae2689b37ec50673e7466b
verdastelo <verda_stelo@hotmail.com>
2016-02-18 21:42:49 +0530

    exercise11.cpp prints all the prime numbers between 7 and 100.

--- ch4/exercises/exercise11.cpp
+++ ch4/exercises/exercise11.cpp
@@ -1,62 +1,70 @@
 /*
-    Find all the prime numbers between 1 and 100.
-    Store the primes in a vector.
-*/
-
-/*
-    Strategy: 
-    1. Check if a number is even or odd. 
-    2. If it is even, it cannot be prime.
-    3. Move to the next number. Repeat step 1. 
-    4. If it is odd, divide it by the first prime number in your list.
-    5. If it divides (the modulus is zero), move to the next number and
-    repeat step 1. 
-    6. If the modulus is not zero, divide the number by the next prime
-    number in the list. Follow step 5 if the modulus is zero and step 
-    6 if the modulus is non-zero.
-    7. Repeat step 6 until the list of prime numbers is exhausted. 
-    8. Add the number to the prime numbers' list if the modulus remains
-    non-zero. 
-    9. Move to step 1. 
+    This is an ugly program. 
+    But it works! 
 */
 
 #include <iostream> 
+#include <cmath> 
 #include <vector> 
+#include <algorithm> 
+
+// divide n by all the prime numbers
+// store the modulus in a vector
+// sort the vector 
+// if modulus is for the first number, n is not a prime
+// if it not, n is a prime 
 
 using namespace std; 
 
-int main()
+int finds_a_prime(int n)
 {
-    // A list of primes. 
-    vector <int> primes;
+    // A list of first three prime numbers. 
+    vector <int> primes; 
+    
     primes.push_back(2);
     primes.push_back(3);
     primes.push_back(5);
     
-    int n = 6; 
+    // A vector for storing the results of the division between
+    // a number "n" and the first three prime numbers. 
+    vector <double> check_primes; 
+    // A number to store the results of the division between
+    // a number "n" and the first three prime numbers before
+    // the result is transferred to check_primes. 
+    double prime_store; 
     
-    // As long as n is less than 100.
-    while (n < 100) { 
-    // Find out if n is even or odd. 
-        if ((n % 2) == 0) { 
-    // If it's even, increment is by 1 and 
-    // check if it is still less than 100. 
-            n++;       
+    // n is the number whose primeness has to be checked. 
+    
+    // If n is even, it cannot be prime. 
+    if ((n % 2) == 0) {
+        //cout << n << " is not a prime.\n";
+        }
+    // If n is odd, it will be divided by all the prime numbers
+    // in our list and the results will be stored in a vector 
+    // check_primes.   
+    else 
+        {
+        for (int i = 0; i < primes.size(); i++) {
+            prime_store = (n % primes[i]); 
+            check_primes.push_back (prime_store); 
             } 
-    // If it's odd. 
-       else 
-    // Divide it by the first prime number in your list.
-       int prime_div_mod; // prime division modulus 
-       prime_div_mod = (n % primes[0]); 
-    // If it's a perfect division (mod == 0), increment n by 1 and
-    // check if n is still less than 100. 
-       if (prime_div_mod == 0) {
-           n++; 
-           }
-     // If the modulus is not 0, divide n by the next
-     // prime number in the list. 
-        prime_div_mod = (n % primes[1]); 
-         
-       else  
+            
+        sort (check_primes.begin(), check_primes.end()); 
+        
+        if (check_primes[0] == 0) {
+            //cout << n << " is not a prime."; 
+            }
+        else 
+            cout << n << " is a prime.";
+        }
+        cout << '\n';
+} 
+
+int main()
+{
+    for (int i = 5; i < 100; i++) {
+        finds_a_prime(i);
+        }
+        
     return 0;
-}
+} 
