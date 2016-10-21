eb4ceaddfe0f8972d07dbda39877415c91f165ac
verdastelo <verda_stelo@hotmail.com>
2016-10-21 06:08:10 +0530

    Pre- and post-conditions

--- ch5/revision/prepostcond.cpp
+++ ch5/revision/prepostcond.cpp
@@ -1,16 +1,11 @@
 // prepostcond.cpp; Write a program with a loop and pre- and post-conditions.
 // verdastelo; 0.1;
-// Couldn't call the square_root function. So used an alternative to
-// to use pre- and post-conditions. It doesn't make sense, I know.
 
 #include "std_lib_facilities.h"
 
 int main() 
 try {
-  cout << "This program finds square roots." << endl;
-  cout << "Enter a positive number (n) to see its square root";
-  cout << " and the square roots of all numbers in the range ";
-  cout << "[n+10)" << endl;
+  cout << "This program finds if a number is greater than 100." << endl;
   cout << "Enter your number: "; 
   double n = 0;
   double result = 0;
@@ -18,7 +13,7 @@ try {
   if (n < 0) error ("Your number is negative.");
   for (int i = n; i < n+10; ++i) {
     result = i-100;
-    cout << i << "'s square root is " << result << endl;
+    cout << i << " minus 10 is " << result << endl;
     if (result < 0) error ("Something is wrong in the loop.");
   }
 
