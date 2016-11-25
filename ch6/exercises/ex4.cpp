a68c5a2c8e1ebf4658afa0c9f310a69261c5ff82
verdastelo <verda_stelo@hotmail.com>
2016-11-25 06:01:35 +0530

    Commenced ex4

---
+++ ch6/exercises/ex4.cpp
@@ -0,0 +1,14 @@
+/*
+    ex4.cpp: Rework exercise 19 from chapter 4. Use a Token-like Name_value
+    instead of two vectors.
+    Chapter 6. Exercise 4.
+    ２０１６年１１月２５日
+*/
+
+#include "std_lib_facilities.h"
+
+int main() 
+{
+    cout << "Hi!" << endl;
+    return 0;
+} 
