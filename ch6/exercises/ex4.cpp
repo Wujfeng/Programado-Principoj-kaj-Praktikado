33b53ae9101382133029e472c8917176d432a46b
verdastelo <verda_stelo@hotmail.com>
2016-11-26 07:56:42 +0530

    Work on ex4.cpp

--- ch6/exercises/ex4.cpp
+++ ch6/exercises/ex4.cpp
@@ -1,14 +1,43 @@
 /*
-    ex4.cpp: Rework exercise 19 from chapter 4. Use a Token-like Name_value
-    instead of two vectors.
+    ex4.cpp: Rework exercise 19 from chapter 4. 
+    Use a Token-like Name_value instead of two vectors.
     Chapter 6. Exercise 4.
     ２０１６年１１月２５日
 */
 
 #include "std_lib_facilities.h"
 
+class Name_value {
+public:
+    string name;
+    double score;	
+    Name_value (string str, double val)
+        :name (str), score (val) {} 
+};
+
 int main() 
-{
-    cout << "Hi!" << endl;
+try { 
+    vector <Name_value> nvVector;
+    for (int i = 0; i < 10; ++i) {
+        double d = 0.0;
+        d += i;
+        nvElement ("Chetan", d);
+        nvVector.push_back (nvElement); 
+    }
+
+    for (int i = 0; i < nvVector.size(); ++i) 
+        cout << nvElement.name << " " << nvElement.score << endl;
+
     return 0;
-} 
+}
+
+catch (exception &e) {
+    cerr << "error: " << e.what() << '\n';
+    return -1;
+}
+
+catch (...) {
+    cerr << "Oops! Something unexpected happened somewhere." << '\n';
+    return -2;
+}
+ 
