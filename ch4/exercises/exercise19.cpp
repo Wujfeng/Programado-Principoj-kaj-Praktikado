8f06a6c22c40f775364e9e3635913a60c9c0b2d3
verdastelo <verda_stelo@hotmail.com>
2016-11-27 21:40:10 +0530

    Completed the exercise.

--- ch4/exercises/exercise19.cpp
+++ ch4/exercises/exercise19.cpp
@@ -1,12 +1,13 @@
-#include <iostream> 
-#include <vector> 
-#include <string> 
-#include <algorithm>
+/*
+    exercise19.cpp
+    It works now.
+    Date: 2016-11-27
+*/
 
-using namespace std; 
+#include "std_lib_facilities.h"
 
 int main()
-{
+try {
     int score;
     vector <int> scores; 
      
@@ -16,18 +17,14 @@ int main()
     cout << "Enter a name followed by scores: "; 
     
     while (cin >> name >> score) {
-    
-        for (int i = 1; i < names.size(); ++i) {
-            if (name == names[i]) {
-            cout << "Error.";
-            }
-         }
-    
-        names.push_back(name);
-        scores.push_back(score); 
+        names.push_back (name);
+        scores.push_back (score);
 
-        }
-        
+        sort (names.begin(), names.end());
+
+        for (int i = 1; i < names.size(); ++i) 
+            if (names[i] == names[i-1]) error ("Can't enter the same name twice.");
+    }       
 
     
     for (int i = 0; i < names.size(); i++) {
@@ -35,4 +32,14 @@ int main()
         }
         
      return 0;
+}
+
+catch (exception& e) {
+    cerr << "error: " << e.what() << '\n';
+    return 1;
+}
+
+catch (...) {
+    cerr << "an unknown exception" << '\n';
+    return 2;
 }
